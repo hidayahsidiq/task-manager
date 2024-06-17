dcbf2272caef0ce7526de78a03ada1e025a84ca5
hidayahsidiq <muhamads03@gmail.com>
2024-06-17 15:56:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -72,7 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function showWeather(position) {
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const apiKey = 'OpenWeatherMap API key'; // Replace with your OpenWeatherMap API key
+    const apiKey = 'b129c14c640ddb12ac3f7dc22abc572d'; // Replace with your OpenWeatherMap API key
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
 
     fetch(url)
@@ -155,3 +155,26 @@ document.getElementById('new-task-button').addEventListener('click', function() 
     }
   });
 
+  function openTab(evt, tabName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+
+    // Get the element with id="defaultOpen" and click on it
+    document.getElementById("defaultOpen").click();
+  }
